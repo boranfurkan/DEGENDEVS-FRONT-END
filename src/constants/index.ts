d29851f2d0b88d068e8e35eea5331e81e2a4d8af
boranfurkan <furkanboran123@gmail.com>
2024-03-07 17:58:22 +0000

    intro section completed.

---
+++ src/constants/index.ts
@@ -0,0 +1,18 @@
+export const NavbarItems = [
+  {
+    name: "About",
+    section: "about",
+  },
+  {
+    name: "Work",
+    section: "work",
+  },
+  {
+    name: "Reviews",
+    section: "reviews",
+  },
+  {
+    name: "Contact",
+    section: "contact",
+  },
+];
