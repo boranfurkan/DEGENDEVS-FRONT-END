3a867d706a86c2eb56171568d5e289c96587bba4
boranfurkan <furkanboran123@gmail.com>
2024-03-07 18:34:29 +0000

    fix: overflow x

--- src/components/shared/intro-section/IntroSection.tsx
+++ src/components/shared/intro-section/IntroSection.tsx
@@ -5,7 +5,7 @@ import Navbar from "../Navbar";
 const IntroSection = () => {
   return (
     <div
-      className={`h-screen w-screen px-10 py-5 relative ${styles.bgAnimationColor} overflow-hidden`}
+      className={`h-screen w-full px-10 py-5 relative ${styles.bgAnimationColor} overflow-hidden`}
     >
       <div className="absolute inline-flex flex-row inset-0 justify-center items-center space-x-4 top-[50px]">
         <div className={`${styles.bigBubble} ${styles.animation1}`}></div>
