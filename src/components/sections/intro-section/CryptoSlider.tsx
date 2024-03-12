984b04b7bd1934096779f4084c05a27fd2bdcce1
boranfurkan <furkanboran123@gmail.com>
2024-03-12 00:07:07 +0000

    feat: crypto slider add.

--- src/components/sections/intro-section/CryptoSlider.tsx
+++ src/components/sections/intro-section/CryptoSlider.tsx
@@ -1,29 +1,150 @@
 import React from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const CryptoSlider = () => {
   return (
     <div className="flex flex-row gap-3">
-      <Image src="/icons/bitcoin.svg" width={50} height={50} alt="Bitcoin" />
-      <Image src="/icons/ethereum.svg" width={50} height={50} alt="Ethereum" />
-      <Image src="/icons/usd.svg" width={50} height={50} alt="Usd" />
-      <Image src="/icons/cosmos.svg" width={50} height={50} alt="Cosmos" />
-      <Image src="/icons/tether.svg" width={50} height={50} alt="Tether" />
-      <Image src="/icons/tron.svg" width={50} height={50} alt="Tron" />
-      <Image src="/icons/binance.svg" width={50} height={50} alt="Binance" />
-
-      <Image
+      <motion.img
+        className="fade-in"
+        src="/icons/bitcoin.svg"
+        width={50}
+        height={50}
+        alt="Bitcoin"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0, duration: 3 }}
+      />
+      <motion.img
+        className="fade-in"
+        src="/icons/ethereum.svg"
+        width={50}
+        height={50}
+        alt="Ethereum"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.2, duration: 3 }}
+      />
+      <motion.img
+        className="fade-in"
+        src="/icons/usd.svg"
+        width={50}
+        height={50}
+        alt="Usd"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.4, duration: 3 }}
+      />
+      <motion.img
+        className="fade-in"
+        src="/icons/cosmos.svg"
+        width={50}
+        height={50}
+        alt="Cosmos"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.6, duration: 3 }}
+      />
+      <motion.img
+        className="fade-in"
+        src="/icons/tether.svg"
+        width={50}
+        height={50}
+        alt="Tether"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.8, duration: 3 }}
+      />
+      <motion.img
+        className="fade-in"
+        src="/icons/tron.svg"
+        width={50}
+        height={50}
+        alt="Tron"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1, duration: 3 }}
+      />
+      <motion.img
+        className="fade-in"
+        src="/icons/binance.svg"
+        width={50}
+        height={50}
+        alt="Binance"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1.2, duration: 3 }}
+      />
+      <motion.img
+        className="fade-in"
         src="/icons/ethereum-classic.svg"
         width={50}
         height={50}
         alt="Ethereum Classic"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1.4, duration: 3 }}
       />
-      <Image src="/icons/dogecoin.svg" width={50} height={50} alt="Dogecoin" />
-      <Image src="/icons/xrp.svg" width={50} height={50} alt="Xrp" />
-      <Image src="/icons/bitcoin.svg" width={50} height={50} alt="Bitcoin" />
-      <Image src="/icons/ethereum.svg" width={50} height={50} alt="Ethereum" />
-      <Image src="/icons/usd.svg" width={50} height={50} alt="Usd" />
-      <Image src="/icons/cosmos.svg" width={50} height={50} alt="Cosmos" />
+      <motion.img
+        className="fade-in"
+        src="/icons/dogecoin.svg"
+        width={50}
+        height={50}
+        alt="Dogecoin"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1.6, duration: 3 }}
+      />
+      <motion.img
+        className="fade-in"
+        src="/icons/xrp.svg"
+        width={50}
+        height={50}
+        alt="Xrp"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1.8, duration: 3 }}
+      />
+      <motion.img
+        className="fade-in"
+        src="/icons/bitcoin.svg"
+        width={50}
+        height={50}
+        alt="Bitcoin"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 2, duration: 3 }}
+      />
+      <motion.img
+        className="fade-in"
+        src="/icons/ethereum.svg"
+        width={50}
+        height={50}
+        alt="Ethereum"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 2.2, duration: 3 }}
+      />
+      <motion.img
+        className="fade-in"
+        src="/icons/usd.svg"
+        width={50}
+        height={50}
+        alt="Usd"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 2.4, duration: 3 }}
+      />
+      <motion.img
+        className="fade-in"
+        src="/icons/cosmos.svg"
+        width={50}
+        height={50}
+        alt="Cosmos"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 2.6, duration: 3 }}
+      />
     </div>
   );
 };
