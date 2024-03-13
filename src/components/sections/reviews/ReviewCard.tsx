9ad4d85ee985e910c44fef35351ff01a5eec0cd8
boranfurkan <furkanboran123@gmail.com>
2024-03-13 01:41:33 +0000

    feat: reviews section complete.

--- src/components/sections/reviews/ReviewCard.tsx
+++ src/components/sections/reviews/ReviewCard.tsx
@@ -1,7 +1,59 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-const ReviewCard = () => {
-  return <div></div>;
+interface ReviewCardProps {
+  name: string;
+  role: string;
+  review: string;
+  img: string;
+  link: string;
+  borderType: "left" | "right" | "middle";
+}
+
+const ReviewCard = ({
+  name,
+  role,
+  review,
+  img,
+  link,
+  borderType,
+}: ReviewCardProps) => {
+  let border = "border-[3px] border-solid border-[#992C11] rounded-[60px]";
+  if (borderType === "left") {
+    border += " rounded-br-none";
+  } else if (borderType === "right") {
+    border += " rounded-bl-none";
+  }
+
+  return (
+    <Link
+      href={link}
+      className={`py-5 px-10 bg-cream flex-col gap-4 h-[261px] w-[410px] flex ${border}`}
+      target="_blank"
+    >
+      <div className="flex flex-row items-center justify-start gap-1.5">
+        <Image
+          src={img}
+          alt="review-image"
+          width={52}
+          height={52}
+          className="rounded-[10px]"
+        />
+        <div className="flex flex-col justify-between">
+          <h3 className="text-xl font-bold leading-6 tracking-normal text-left capitalize text-brown">
+            {name}
+          </h3>
+          <span className="text-base font-medium leading-5 tracking-normal text-left text-primary-200">
+            {role}
+          </span>
+        </div>
+      </div>
+      <p className="text-sm font-medium leading-[21px] tracking-normal text-left text-brown overflow-y-scroll no-scrollbar">
+        &quot;{review}&quot;
+      </p>
+    </Link>
+  );
 };
 
 export default ReviewCard;
---
+++ src/components/sections/reviews/Reviews.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import { Reviews as ReviewsData } from "@/constants";
+import Slider from "infinite-react-carousel";
+import ReviewCard from "./ReviewCard";
+
+const Reviews = () => {
+  const settings = {
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesPerRow: 1,
+    dots: true,
+    className: "!w-full",
+  };
+  return (
+    <div className="flex flex-col gap-10 w-full">
+      <h3 className="text-[64px] font-bold leading-[79px] tracking-normal text-center">
+        What clients say about us
+      </h3>
+      <Slider {...settings}>
+        <div className="!flex items-center justify-center gap-10 w-full">
+          <ReviewCard {...ReviewsData[3]} borderType={"middle"} />
+          <ReviewCard {...ReviewsData[0]} borderType={"middle"} />
+        </div>
+        <div className="!flex items-center justify-center gap-10 w-full">
+          <ReviewCard {...ReviewsData[2]} borderType={"middle"} />
+          <ReviewCard {...ReviewsData[1]} borderType={"middle"} />
+        </div>
+        <div className="!flex items-center justify-center gap-10 w-full">
+          <ReviewCard {...ReviewsData[4]} borderType={"middle"} />
+          <ReviewCard {...ReviewsData[5]} borderType={"middle"} />
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Reviews;
