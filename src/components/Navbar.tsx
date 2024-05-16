5184f651d14898d53406b84ce5a635e60f31027e
boranfurkan <furkanboran123@gmail.com>
2024-05-17 01:03:09 +0300

    fix: small desktop complete for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -51,8 +51,8 @@ const Navbar = () => {
       const sectionHeight = section.offsetHeight;
 
       if (
-        scrollTop >= sectionTop - 100 &&
-        scrollTop < sectionTop + sectionHeight - 100
+        scrollTop >= sectionTop - 200 &&
+        scrollTop < sectionTop + sectionHeight - 200
       ) {
         setActiveSection(section.id);
       }
@@ -65,48 +65,51 @@ const Navbar = () => {
   };
 
   return isSticky ? (
-    <div className="w-full fixed top-5 left-0 z-[99] flex items-center justify-center">
-      <motion.div
-        className="mockup-window w-[50%] h-max glass-lg max-sm:w-[92%] max-sm:py-6"
-        initial="hidden"
-        animate="show"
-        variants={navbarAnimation}
-      >
-        <div className="absolute inset-0 w-full h-max top-[0.7rem]">
-          <nav
-            ref={navRef}
-            className="flex items-center justify-center gap-6 text-white relative"
-          >
-            <div
-              className="absolute inset-0 -top-1 bg-primary-400 bg-opacity-50 z-[-1] rounded-full h-8 w-0 cursor-pointer"
-              style={indicatorStyle}
-            ></div>
-            {NavbarItems.map((item, index) => {
-              return (
-                <Link
-                  href={`#${item.section}`}
-                  key={index}
-                  data-section={item.section}
-                  onClick={() => setActiveSection(item.section)}
-                  className={`text-white transition-colors duration-300 ease-in-out text-base font-semibold tracking-normal cursor-pointer ${
-                    activeSection !== item.section && "hover-effect"
-                  }`}
-                >
-                  {item.name}
-                </Link>
-              );
-            })}
-          </nav>
-        </div>
-        <Image
-          src="/logo.png"
-          alt="logo"
-          width={60}
-          height={30}
-          className="absolute right-3 top-0.5 max-sm:hidden"
-        />
-      </motion.div>
-    </div>
+    <>
+      <div className="h-[48px] w-full bg-transparent"></div>
+      <div className="w-full fixed top-5 left-0 z-[99] flex items-center justify-center">
+        <motion.div
+          className="mockup-window w-[50%] h-max glass-lg max-sm:w-[92%] max-sm:py-6"
+          initial="hidden"
+          animate="show"
+          variants={navbarAnimation}
+        >
+          <div className="absolute inset-0 w-full h-max top-[0.7rem]">
+            <nav
+              ref={navRef}
+              className="flex items-center justify-center gap-6 text-white relative"
+            >
+              <div
+                className="absolute inset-0 -top-1 bg-primary-400 bg-opacity-50 z-[-1] rounded-full h-8 w-0 cursor-pointer"
+                style={indicatorStyle}
+              ></div>
+              {NavbarItems.map((item, index) => {
+                return (
+                  <Link
+                    href={`#${item.section}`}
+                    key={index}
+                    data-section={item.section}
+                    onClick={() => setActiveSection(item.section)}
+                    className={`text-white transition-colors duration-300 ease-in-out text-base font-semibold tracking-normal cursor-pointer ${
+                      activeSection !== item.section && "hover-effect"
+                    }`}
+                  >
+                    {item.name}
+                  </Link>
+                );
+              })}
+            </nav>
+          </div>
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width={60}
+            height={30}
+            className="absolute right-3 top-0.5 max-sm:hidden"
+          />
+        </motion.div>
+      </div>
+    </>
   ) : (
     <div className="mockup-window w-full h-max relative glass max-sm:py-6 min-h-[48px]">
       <div className="absolute inset-0 w-full h-max top-[0.7rem]">
