8007ef35e188d837eee43d1f800b1bd64235f20d
boranfurkan <furkanboran123@gmail.com>
2024-05-16 20:12:06 +0300

    fix: update how it works section for mobile

--- src/components/sections/work/HowItWorks.tsx
+++ src/components/sections/work/HowItWorks.tsx
@@ -79,23 +79,23 @@ const HowItWorks = () => {
   });
 
   return (
-    <div className="flex flex-col gap-16">
+    <div className="flex flex-col gap-16 max-sm:gap-10">
       <h3 className="text-[64px] font-bold leading-[79px] tracking-normal text-center">
         How it works?
       </h3>
-      <div className="flex flex-row gap-24 items-center justify-between">
+      <div className="flex flex-row gap-24 items-center justify-between max-sm:flex-col max-sm:gap-12">
         <DocumentAnimation />
         <div className="flex flex-col gap-14" ref={ref}>
           <motion.div
-            className="px-10 py-5 rounded-[60px] flex flex-col gap-1 bg-primary-300 relative"
+            className="px-10 py-5 rounded-[60px] flex flex-col gap-1 bg-primary-300 relative max-sm:px-4"
             initial="offscreen"
             animate={inView ? "onscreen" : "offscreen"}
             variants={getDivVariantWithDelay(0)}
           >
-            <h2 className="text-5xl font-bold leading-[60px] tracking-normal text-left">
+            <h2 className="text-5xl font-bold leading-[60px] tracking-normal text-left max-sm:text-center">
               1. Plan
             </h2>
-            <p className="text-xl font-medium leading-[30px] tracking-normal text-left">
+            <p className="text-xl font-medium leading-[30px] tracking-normal text-left max-sm:text-center">
               Outline project goals & we align designs with your vision. We work
               with you to understand your business and your customers.
             </p>
@@ -103,27 +103,28 @@ const HowItWorks = () => {
               initial="offscreen"
               animate={inView ? "onscreen" : "offscreen"}
               variants={bulbVariant}
-              className="absolute -top-12 -left-12"
+              className="absolute -top-12 -left-12 max-sm:-left-2 max-sm:-top-6"
             >
               <Image
                 src="/assets/bulb.png"
                 width={108}
                 height={103}
                 alt="bulb"
+                className="max-sm:w-[80px] max-sm:h-auto"
               />
             </motion.div>
           </motion.div>
 
           <motion.div
-            className="px-10 py-5 rounded-[60px] flex flex-col gap-1 bg-primary-300 relative"
+            className="px-10 py-5 rounded-[60px] flex flex-col gap-1 bg-primary-300 relative max-sm:px-4"
             initial="offscreen"
             animate={inView ? "onscreen" : "offscreen"}
             variants={getDivVariantWithDelay(0.8)}
           >
-            <h2 className="text-5xl font-bold leading-[60px] tracking-normal text-left">
+            <h2 className="text-5xl font-bold leading-[60px] tracking-normal text-left max-sm:text-center">
               2. Iterate
             </h2>
-            <p className="text-xl font-medium leading-[30px] tracking-normal text-left">
+            <p className="text-xl font-medium leading-[30px] tracking-normal text-left max-sm:text-center">
               Dive into the design phase and refine through unlimited revisions
               until perfection. We make sure you are 100% satisfied.
             </p>
@@ -131,27 +132,28 @@ const HowItWorks = () => {
               initial="offscreen"
               animate={inView ? "onscreen" : "offscreen"}
               variants={gearVariant}
-              className="absolute -top-12 -left-12"
+              className="absolute -top-12 -left-12 max-sm:-left-2 max-sm:-top-6"
             >
               <Image
                 src="/assets/gear.png"
                 width={108}
                 height={103}
                 alt="gear"
+                className="max-sm:w-[80px] max-sm:h-auto"
               />
             </motion.div>
           </motion.div>
 
           <motion.div
-            className="px-10 py-5 rounded-[60px] flex flex-col gap-1 bg-primary-300 relative"
+            className="px-10 py-5 rounded-[60px] flex flex-col gap-1 bg-primary-300 relative max-sm:px-4"
             initial="offscreen"
             animate={inView ? "onscreen" : "offscreen"}
             variants={getDivVariantWithDelay(1.6)}
           >
-            <h2 className="text-5xl font-bold leading-[60px] tracking-normal text-left">
+            <h2 className="text-5xl font-bold leading-[60px] tracking-normal text-left max-sm:text-center">
               3. Launch
             </h2>
-            <p className="text-xl font-medium leading-[30px] tracking-normal text-left">
+            <p className="text-xl font-medium leading-[30px] tracking-normal text-left max-sm:text-center">
               Finalize your design, implement it, and let your business grow
               with a design that evolves with you.
             </p>
@@ -159,13 +161,14 @@ const HowItWorks = () => {
               initial="offscreen"
               animate={inView ? "onscreen" : "offscreen"}
               variants={rocketVariant}
-              className="absolute -top-12 -left-12"
+              className="absolute -top-12 -left-12 max-sm:-left-2 max-sm:-top-6"
             >
               <Image
                 src="/assets/rocket.png"
                 width={108}
                 height={103}
                 alt="rocket"
+                className="max-sm:w-[80px] max-sm:h-auto"
               />
             </motion.div>
           </motion.div>
