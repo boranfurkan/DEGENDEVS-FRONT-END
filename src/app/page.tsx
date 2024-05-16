6fb1cae9ca8125e62c8f2e5e55ab470592f74607
boranfurkan <furkanboran123@gmail.com>
2024-05-16 20:09:25 +0300

    feat: create mobile design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,16 +12,19 @@ export default function Home() {
       <section id="welcome">
         <IntroSection />
       </section>
-      <div className="px-[7.5rem] py-12 flex flex-col gap-44 justify-center items-center w-full">
-        <section id="about" className="max-w-[1272px]">
+      <div className="px-[7.5rem] py-12 flex flex-col gap-44 justify-center items-center w-full max-sm:px-5 max-sm:gap-28">
+        <section id="about" className="max-w-[1272px] max-sm:w-full">
           <About />
         </section>
-        <section id="work" className="max-w-[1272px] flex flex-col gap-44 ">
+        <section
+          id="work"
+          className="max-w-[1272px] flex flex-col gap-44 max-sm:w-full max-sm:gap-28"
+        >
           <Work />
           <HowItWorks />
           <ProjectsSlider />
         </section>
-        <section id="reviews" className="w-[1272px]">
+        <section id="reviews" className="w-[1272px] max-sm:w-full">
           <Reviews />
         </section>
       </div>
