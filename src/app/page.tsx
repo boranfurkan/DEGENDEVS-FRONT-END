2565f1466079fdc25cdf60de2d98c5bad93aa379
boranfurkan <furkanboran123@gmail.com>
2024-05-17 01:02:51 +0300

    fix: small desktop version complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,20 +13,20 @@ export default function Home() {
       <section id="welcome">
         <IntroSection />
       </section>
-      <div className="px-[7.5rem] py-12 flex flex-col gap-44 justify-center items-center w-full max-sm:px-5 max-sm:gap-28">
-        <section id="about" className="max-w-[1272px] max-sm:w-full">
+      <div className="px-[7.5rem] py-12 flex flex-col gap-44 justify-center items-center w-full max-sm:px-5 max-sm:gap-28 max-xl:px-10 max-xl:gap-36">
+        <section id="about" className="max-w-[1272px] max-xl:w-full ">
           <About />
         </section>
         <section
           id="work"
-          className="max-w-[1272px] flex flex-col gap-44 max-sm:w-full max-sm:gap-28"
+          className="max-w-[1272px] flex flex-col gap-44 max-xl:w-full max-sm:gap-28 max-xl:gap-36"
         >
           <Work />
           <WhatWeWorkOn />
           <HowItWorks />
           <ProjectsSlider />
         </section>
-        <section id="reviews" className="w-[1272px] max-sm:w-full">
+        <section id="reviews" className="w-[1272px] max-xl:w-full">
           <Reviews />
         </section>
       </div>
